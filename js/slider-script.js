a9fdbd353ec31b456c387a4c9a03be4c5d15cc85
Vladimir Shumskiy <shmsk.vldmr@gmail.com>
2019-09-19 01:49:48 +0300

    Complete slider block

---
+++ js/slider-script.js
@@ -0,0 +1,26 @@
+let vm = new Vue({
+	el: "#slider",
+	data: {
+		pageWidth: 760
+	},
+	methods: {
+		firstPage: function(){
+			this.choosePage(0);
+		},
+		secondPage: function(){
+			this.choosePage(1);
+		},
+		thirdPage: function(){
+			this.choosePage(2);
+		},
+		choosePage: function(num){
+			let sliderLine = document.getElementById("slider-line");
+			sliderLine.style.transform = 'translateX(-' + this.pageWidth*num + 'px)';
+			let square = document.querySelectorAll(".choice-square");
+			for (var i = 0; i < square.length; i++) {
+				square[i].style.background = 'none';
+			}
+			square[num].style.background = '#212121';
+		}
+	}
+})
